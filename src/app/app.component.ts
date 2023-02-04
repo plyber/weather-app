260f6e56cef0f1a41cde3f7a35e435d0c2f387d0
Oliviu <dienes.oliviu@gmail.com>
2023-02-05 01:48:42 +0200

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit{
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  weatherData?: WeatherData;
+  
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Timisoara')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response)
+      }
+    });
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'f6cb4533cdmsh33aa86f78483433p158a28jsnac42de37dfa3'
+
+};
